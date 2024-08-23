fec2d7ac4b935e02a13fb855712742d9d960e91b
jcv-dev <valenciarivasjuancamilo@gmail.com>
2024-08-23 15:13:43 -0500

    Fix caja-herramientas

---
+++ caja-herramientas/src/themes/Maintheme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const Maintheme = createTheme({
+    palette: {
+        primary: {
+            main: '#ba0017',
+        },
+    },
+});
+
+export default Maintheme;
