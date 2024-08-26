7ce49928eebef324cb0ce9bebceac6ea58de4b99
jcv-dev <valenciarivasjuancamilo@gmail.com>
2024-08-25 22:40:50 -0500

    Fix + run scripts

--- herramientas-externas/rutas-atencion/src/App.js
+++ herramientas-externas/rutas-atencion/src/App.js
@@ -1,21 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './componentes/Header';
 
 function App() {
-
   return (
-
-      <div>
-
-          <Header />
-
-          {/* Other components or content */}
-
-      </div>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and semaforo to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-
 }
 
 export default App;
-
