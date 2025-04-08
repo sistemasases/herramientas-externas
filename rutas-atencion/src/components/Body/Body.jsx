fbae2f9079780715e07f978c890151dc3f6cde0a
jcv-dev <valenciarivasjuancamilo@gmail.com>
2025-04-07 22:42:28 -0500

    Avance rutas

--- rutas-atencion/src/components/Body/Body.jsx
+++ rutas-atencion/src/components/Body/Body.jsx
@@ -1,22 +1,41 @@
-import './Body.css';
-import uvArdilla from '../../assets/uvArdilla.png'
-import React from 'react';
+import "./Body.css";
+import uvArdilla from "../../assets/uvArdilla.png";
+import bgArbol from "../../assets/bgArbol.svg";
+import chica from "../../assets/chica.svg";
+import cartel from "../../assets/cartel.svg";
+import React from "react";
+
+const info = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function Body() {
   return (
-    <main className="main-content">
-        <img id="bgArbol" src={uvArdilla} alt="UvArdilla" />
-        <img id="uvArdilla" src={uvArdilla} alt="UvArdilla" />
-      <div className="columns-container">
-        <div className='tooltip-column'>
-          <h2>Tooltip</h2>
-          <p>Tooltip description</p>
-        </div>
-        <div className="dimensions-column">
-          <h2>Dimensions</h2>
-        </div>
+    <div className="page-container">
+      {/* Background container with absolute positioning */}
+      <div className="background-container">
+        <img src={bgArbol} alt="Background Tree" className="background-image" />
       </div>
-    </main>
+      {/* Content area */}
+      <main className="main-content">
+        <div className="columns-container">
+          <div className="tooltip-column">
+            <div className="tooltip-img">
+              <img id="chica" src={chica} alt="Chica" />
+            </div>
+          </div>
+          <div className="dimensions-column">
+            <div className="cartels-grid">
+              {info.map((_, index) => (
+                <div className="cartel-wrapper" key={index}>
+                  <img className="cartel-item" src={cartel} alt="Cartel" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+      {/* Squirrel image positioned absolutely */}
+      <img id="uvArdilla" src={uvArdilla} alt="UvArdilla" />
+    </div>
   );
 }
 
